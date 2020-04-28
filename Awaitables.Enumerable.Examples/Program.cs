cf43d64f44f70d842a4097bef919179deeb93614
yair halberstadt <yairhalberstadt@gmail.com>
2020-04-28 18:26:13 +0300

    Add AwaitableEnumerable example

---
+++ Awaitables.Enumerable.Examples/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Awaitables.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exception = new AggregateException(
+                new AggregateException(
+                    new Exception("Bad stuff")), 
+                new InvalidCastException("Bad cast!!"), 
+                new AggregateException(
+                    new InvalidOperationException("Bad Operation"), 
+                    new AggregateException(
+                        new ArgumentNullException("name"), 
+                        new NullReferenceException("another null"))));
+
+            FlattenAndLog(exception);
+        }
+
+        static async AwaitableEnumerable<Exception> FlattenAndLog(Exception exception)
+        {
+            while(exception is AggregateException aggregateException)
+            {
+                exception = await aggregateException.InnerExceptions.ToAwaitable();
+                if (!(exception is AggregateException))
+                {
+                    Console.WriteLine(exception.Message);
+                    return exception;
+                }
+            }
+            throw new InvalidOperationException("Unreachable");
+        }
+    }
+}

5ac0886ae9034b2676fb73ea00e11f73126d3d85
yair halberstadt <yairhalberstadt@gmail.com>
2020-04-28 18:13:14 +0300

    Added examples

---
+++ Awaitables.Option.Example/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Awaitables.Example
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var program = GetSelectedProgram();
+            if (program.HasValue)
+            {
+                program.Value();
+            }
+            else
+            {
+                Console.WriteLine("Invalid argument");
+            }
+            async Option<Action> GetSelectedProgram()
+            {
+                var arg = await args.ElementAt(0);
+                var asInt = await arg.TryParseInt();
+                return await _programs.TryGetValue(asInt);
+            }
+        }
+
+        public static Dictionary<int, Action> _programs = GeneratePrograms();
+
+        private static Dictionary<int, Action> GeneratePrograms() => new Dictionary<int, Action>
+        {
+            { 1, () => Console.WriteLine("Play chess") },
+            { 2, () => Console.WriteLine("Solve World Hunger") },
+            { 3, () => Console.WriteLine("Win a noble prize") },
+        };
+    }
+
+    public static class Extensions
+    {
+        public static Option<T> ElementAt<T>(this IReadOnlyList<T> list, int index)
+            => list.Count > index ? Option.Some(list[index]) : Option.None<T>();
+        public static Option<TValue> TryGetValue<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key)
+            => dictionary.TryGetValue(key, out var value) ? Option.Some(value) : Option.None<TValue>();
+        public static Option<int> TryParseInt(this string str)
+            => int.TryParse(str, out var value) ? Option.Some(value) : Option.None<int>();
+    }
+}
---
+++ Awaitables.Result.Examples/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Awaitables.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = UpdateDbWithRetry();
+
+            if (result.IsSuccessful)
+            {
+                Console.WriteLine($"Wrote {result.Value} items to db");
+            }
+            else
+            {
+                Console.WriteLine(result.Exception);
+            }
+
+            Result<int> UpdateDbWithRetry()
+            {
+                return UpdateDb() switch
+                {
+                    { IsSuccessful: true } result => result,
+                    { Exception: TimeoutException _ } => UpdateDbWithRetry(),
+                    var result => result,
+                };
+            }
+
+            async Result<int> UpdateDb()
+            {
+                var connectionString = "MyConnectionString";
+                var integers = await Db.Query<int>(connectionString, "Select * from Integers");
+                var squares = integers.Select(x => x ^ 2);
+                return await Db.Write(connectionString, squares);
+            }
+        }
+    }
+
+    public static class Db
+    {
+        private static readonly Random _random = new Random();
+        public static async Result<IEnumerable<TResult>> Query<TResult>(string connectionString, string query) => _random.Next() % 2 == 0
+            ? throw new TimeoutException("could not connect")
+            : Enumerable.Range(0, _random.Next() % 10).Cast<TResult>();
+
+        public static async Result<int> Write<T>(string connectionString, IEnumerable<T> data) => data.Count() > 5
+            ? throw new InvalidDataException("Too many items!!!")
+            : data.Count();
+    }
+
+}
